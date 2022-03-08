915602a6eada008bed0d55ffaf4ab7fea0ec3a67
dianaellis <101216237+dianaellis@users.noreply.github.com>
2022-03-08 12:23:16 -0800

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,23 @@
+package javaPackage;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c = a+b;
+		int d = a*b;
+		int e= b/a;
+		int f =b-a;
+		
+		int g= b % a;
+		System.out.println("Addition "+c);
+		System.out.println("Product "+d);
+		System.out.println("Division "+e);
+		System.out.println("Substract "+f);
+		System.out.println("Modulus "+g);
+
+	}
+
+}
